b632bf695b5546bfc6723f5b061ede594b15a8e7
Nisha Shirawala <nishashirawala@gmail.com>
2020-05-25 18:20:32 -0400

    Reset done Player disabled after played once. Enabled on deck click only

--- app/src/main/java/com/playing/cardfun/MainActivity.kt
+++ app/src/main/java/com/playing/cardfun/MainActivity.kt
@@ -13,55 +13,142 @@ class MainActivity : AppCompatActivity() {
     private var cards = ArrayList<Card>()
     private val player1Cards = ArrayList<Card>()
     private val player2Cards = ArrayList<Card>()
+    lateinit var grayCard: ImageView
+    private var player1Played: Boolean = false
+    private var player2Played: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        grayCard = ImageView(applicationContext)
+
         distributeCards()
 
-        player1card0.setOnClickListener{ player1CardClicked(player1card0, R.string.p1c0) }
-        player1card1.setOnClickListener{ player1CardClicked(player1card1, R.string.p1c1) }
-        player1card2.setOnClickListener{ player1CardClicked(player1card2, R.string.p1c2) }
-        player1card3.setOnClickListener{ player1CardClicked(player1card3, R.string.p1c3) }
-        player1card4.setOnClickListener{ player1CardClicked(player1card4, R.string.p1c4) }
+        player1card0.setOnClickListener{ player1CardClicked(player1card0, R.string.p1card) }
+        player1card1.setOnClickListener{ player1CardClicked(player1card1, R.string.p1card) }
+        player1card2.setOnClickListener{ player1CardClicked(player1card2, R.string.p1card) }
+        player1card3.setOnClickListener{ player1CardClicked(player1card3, R.string.p1card) }
+        player1card4.setOnClickListener{ player1CardClicked(player1card4, R.string.p1card) }
 
-        player2card0.setOnClickListener{ player2CardClicked(player2card0, R.string.p2c0) }
-        player2card1.setOnClickListener{ player2CardClicked(player2card1, R.string.p2c1) }
-        player2card2.setOnClickListener{ player2CardClicked(player2card2, R.string.p2c2) }
-        player2card3.setOnClickListener{ player2CardClicked(player2card3, R.string.p2c3) }
-        player2card4.setOnClickListener{ player2CardClicked(player2card4, R.string.p2c4) }
+        player2card0.setOnClickListener{ player2CardClicked(player2card0, R.string.p2card) }
+        player2card1.setOnClickListener{ player2CardClicked(player2card1, R.string.p2card) }
+        player2card2.setOnClickListener{ player2CardClicked(player2card2, R.string.p2card) }
+        player2card3.setOnClickListener{ player2CardClicked(player2card3, R.string.p2card) }
+        player2card4.setOnClickListener{ player2CardClicked(player2card4, R.string.p2card) }
+
+        deck.setOnClickListener {
+            print("deck clicked " + player1Played +  "  " + player2Played)
+            cards.shuffle()
+            val newCard = cards.get(0)
+            if(player1Played) {
+                grayCard.setImageResource(newCard.imageId)
+                grayCard.setTag(R.string.p1card, newCard)
+                player1Cards.add(newCard)
+                player1Played = false
+                cards.remove(newCard)
+                setPlayerEnabled("player1", false)
+                setPlayerEnabled("player2", true)
+            } else if (player2Played) {
+                grayCard.setImageResource(newCard.imageId)
+                grayCard.setTag(R.string.p2card, newCard)
+                player2Cards.add(newCard)
+                player2Played = false
+                cards.remove(newCard)
+                setPlayerEnabled("player2", false)
+                setPlayerEnabled("player1", true)
+            }
+        }
 
         show_btn.setOnClickListener{
             println(player1Cards.size)
-            val t = Toast.makeText(applicationContext, " Points " + player1Cards.sumBy{ card -> card.points }, Toast.LENGTH_LONG)
+            println(player2Cards.size)
+
+            val t = Toast.makeText(applicationContext, " Points player1 = " + player1Cards.sumBy{ card -> card.points } + " player2 = " + player2Cards.sumBy { card -> card.points }, Toast.LENGTH_LONG)
             t.show()
+        }
+
+        usedCardPlayer1.setOnClickListener {
+            val newCard: Card = usedCardPlayer1.getTag(R.string.p1card) as Card
+
+            if(player1Played) {
+                grayCard.setImageResource(newCard.imageId)
+                grayCard.setTag(R.string.p1card, newCard)
+
+                player1Cards.add(newCard)
+                usedCardPlayer1.setImageResource(R.drawable.red_back)
+                usedCardPlayer1.setTag(R.string.p1card, null)
+                player1Played = false
+            } else if (player2Played) {
+                grayCard.setImageResource(newCard.imageId)
+                grayCard.setTag(R.string.p1card, newCard)
+
+                player2Cards.add(newCard)
+                usedCardPlayer1.setImageResource(R.drawable.red_back)
+                usedCardPlayer1.setTag(R.string.p2card, null)
+                player2Played = false
+            }
+        }
+
+        usedCardPlayer2.setOnClickListener {
+            val newCard: Card = usedCardPlayer2.getTag(R.string.p2card) as Card
+
+            grayCard.setImageResource(newCard.imageId)
+            grayCard.setTag(R.string.p2card, newCard)
+
+            if(player1Played) {
+                player1Cards.add(newCard)
+                usedCardPlayer2.setImageResource(R.drawable.red_back)
+                usedCardPlayer2.setTag(R.string.p1card, null)
+            } else if (player2Played) {
+                player2Cards.add(newCard)
+                usedCardPlayer2.setImageResource(R.drawable.red_back)
+                usedCardPlayer2.setTag(R.string.p2card, null)
+            }
+        }
+    }
+
+    private fun setPlayerEnabled(playerStr: String, isEnabled: Boolean) {
+        for (x in 0.. 4) {
+            var playerCardStr = playerStr+"card$x"
+            var playerVar: ImageView =
+                findViewById(resources.getIdentifier(playerCardStr, "id", packageName))
+            playerVar.isEnabled = isEnabled
         }
     }
 
     private fun player1CardClicked(playerCard: ImageView, tag: Int) {
-        usedCardPlayer1.setImageDrawable(playerCard.drawable)
-        cards.shuffle()
-        val newCard = cards.get(0)
-        playerCard.setImageResource(newCard.imageId)
-        player1Cards.remove(playerCard.getTag(tag))
+        if(playerCard.getTag(tag) != R.drawable.gray_back && !player1Played) {
+            player2Played = false
+            player1Played = true
+            player1Cards.remove(playerCard.getTag(tag))
 
-        player1Cards.add(newCard)
-        playerCard.setTag(tag, newCard)
-        cards.remove(newCard)
+            grayCard = playerCard
+            usedCardPlayer1.setImageDrawable(playerCard.drawable)
+            usedCardPlayer1.setTag(tag, playerCard.getTag(tag) as Card)
+
+            playerCard.setImageResource(R.drawable.gray_back)
+            playerCard.setTag(tag, R.drawable.gray_back)
+            grayCard.setTag(tag, R.drawable.gray_back)
+        }
     }
 
     private fun player2CardClicked(playerCard: ImageView, tag: Int) {
-        usedCardPlayer2.setImageDrawable(playerCard.drawable)
-        cards.shuffle()
-        val newCard = cards.get(0)
-        playerCard.setImageResource(newCard.imageId)
-        player2Cards.remove(playerCard.getTag(tag))
+        if(playerCard.getTag(tag) != R.drawable.gray_back && !player2Played) {
+            player1Played = false
+            player2Played = true
+            player2Cards.remove(playerCard.getTag(tag))
 
-        player2Cards.add(newCard)
-        playerCard.setTag(tag, newCard)
-        cards.remove(newCard)
+            grayCard = playerCard
+            usedCardPlayer2.setImageDrawable(playerCard.drawable)
+            usedCardPlayer2.setTag(tag, playerCard.getTag(tag) as Card)
+
+            playerCard.setImageResource(R.drawable.gray_back)
+            playerCard.setTag(tag, R.drawable.gray_back)
+            grayCard.setTag(tag, R.drawable.gray_back)
+        }
     }
-
 
     private fun createCardList() : ArrayList<Card> {
         val cardList = ArrayList<Card>()
@@ -137,14 +224,23 @@ class MainActivity : AppCompatActivity() {
         cardList.add(Card("heart_2", 2, R.drawable.heart2))
         cardList.add(Card("spade_2", 2, R.drawable.spade2))
 
+        cardList.shuffle()
+
         return cardList
     }
 
     private fun distributeCards() {
         shuffle_btn.setOnClickListener{
+            // Reset everything
             cards = createCardList()
             player1Cards.clear()
             player2Cards.clear()
+            usedCardPlayer1.setImageResource(R.drawable.red_back)
+            usedCardPlayer1.setTag(R.string.p2card, null)
+            usedCardPlayer2.setImageResource(R.drawable.red_back)
+            usedCardPlayer2.setTag(R.string.p2card, null)
+            player1Played = false
+            player2Played = false
 
             cards.shuffle();
 
@@ -154,26 +250,26 @@ class MainActivity : AppCompatActivity() {
             player2Cards.addAll(listOf(cards[5], cards[6], cards[7], cards[8], cards[9]))
 
             player1card0.setImageResource(cards[0].imageId)
-            player1card0.setTag(R.string.p1c0, cards[0])
+            player1card0.setTag(R.string.p1card, cards[0])
             player1card1.setImageResource(cards[1].imageId)
-            player1card1.setTag(R.string.p1c1, cards[1])
+            player1card1.setTag(R.string.p1card, cards[1])
             player1card2.setImageResource(cards[2].imageId)
-            player1card2.setTag(R.string.p1c2, cards[3])
+            player1card2.setTag(R.string.p1card, cards[3])
             player1card3.setImageResource(cards[3].imageId)
-            player1card3.setTag(R.string.p1c3, cards[3])
+            player1card3.setTag(R.string.p1card, cards[3])
             player1card4.setImageResource(cards[4].imageId)
-            player1card4.setTag(R.string.p1c4, cards[4])
+            player1card4.setTag(R.string.p1card, cards[4])
 
             player2card0.setImageResource(cards[5].imageId)
-            player2card0.setTag(R.string.p2c0, cards[5])
+            player2card0.setTag(R.string.p2card, cards[5])
             player2card1.setImageResource(cards[6].imageId)
-            player2card1.setTag(R.string.p2c1, cards[6])
+            player2card1.setTag(R.string.p2card, cards[6])
             player2card2.setImageResource(cards[7].imageId)
-            player2card2.setTag(R.string.p2c2, cards[7])
+            player2card2.setTag(R.string.p2card, cards[7])
             player2card3.setImageResource(cards[8].imageId)
-            player2card3.setTag(R.string.p2c3, cards[8])
+            player2card3.setTag(R.string.p2card, cards[8])
             player2card4.setImageResource(cards[9].imageId)
-            player2card4.setTag(R.string.p2c3, cards[9])
+            player2card4.setTag(R.string.p2card, cards[9])
 
             cards.removeAll(usedCards)
         }
